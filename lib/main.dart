6e4ff5004aef50166de1eb71b5d196172dc25193
Patricio Bruno Da Costa <pbrunorb@hotmail.com>
2024-09-05 04:26:40 -0300

    splash screen and logo also PWA configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:card_keeper/main_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
   runApp(const MainApp());
 }
 
--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,11 +1,17 @@
 import 'package:card_keeper/screens/main_page/main.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, // Transparent status bar
+      statusBarBrightness: Brightness.dark, // Dark text for status bar
+    ));
+
     return const MaterialApp(
         title: 'PokéDeck',
         home: MainPage());
