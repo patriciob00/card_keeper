8eef1052fcaa3d6bb8f10e06087655d37a121546
Patricio Bruno Da Costa <pbrunorb@hotmail.com>
2024-09-08 07:03:08 -0300

    new bottom bar

--- lib/screens/main_page/components/main_bottom_tabs.dart
+++ lib/screens/main_page/components/main_bottom_tabs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class MainBottomTabs extends StatelessWidget {
   final int currentIdx;
@@ -10,33 +11,81 @@ class MainBottomTabs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      backgroundColor: const Color.fromARGB(255, 240, 0, 0),
+      elevation: 0,
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Colors.black,
       currentIndex: currentIdx,
       onTap: (idx) => onTap(idx),
-      items: const [
+      items: [
         BottomNavigationBarItem(
-            label: '',
-            tooltip: 'Buscar',
-            activeIcon: Icon(
-              Icons.search,
-              color: Color.fromARGB(255, 34, 34, 36),
-            ),
-            icon: Icon(
-              Icons.search,
-              color: Colors.white,
-            )),
+          label: '',
+          tooltip: 'Home',
+          activeIcon: SvgPicture.asset(
+            'assets/svg/home_icon.svg',
+            width: 50,
+            semanticsLabel: 'home',
+            colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+          ),
+          icon: SvgPicture.asset(
+            'assets/svg/home_icon.svg',
+            width: 50,
+            semanticsLabel: 'search of cards',
+            colorFilter:
+                const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+          ),
+        ),
         BottomNavigationBarItem(
-            label: '',
-            tooltip: 'Favoritos',
-            activeIcon: Icon(
-              Icons.favorite,
-              color: Color.fromARGB(255, 34, 34, 36),
-            ),
-            icon: Icon(
-              Icons.favorite_outline,
-              color: Colors.white,
-            )),
+          label: '',
+          tooltip: 'Search',
+          activeIcon: SvgPicture.asset(
+            'assets/svg/search_icon.svg',
+            width: 50,
+            semanticsLabel: 'search of cards',
+            colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+          ),
+          icon: SvgPicture.asset(
+            'assets/svg/search_icon.svg',
+            width: 50,
+            semanticsLabel: 'search of cards',
+            colorFilter:
+                const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+          ),
+        ),
+        BottomNavigationBarItem(
+          label: '',
+          tooltip: 'Lista',
+          activeIcon: SvgPicture.asset(
+            'assets/svg/list_cards.svg',
+            width: 50,
+            semanticsLabel: 'list of cards',
+            colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+          ),
+          icon: SvgPicture.asset(
+            'assets/svg/list_cards.svg',
+            width: 50,
+            semanticsLabel: 'list of cards',
+            colorFilter:
+                const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+          ),
+        ),
+        BottomNavigationBarItem(
+          label: '',
+          tooltip: 'Decks',
+          activeIcon: SvgPicture.asset(
+            'assets/svg/deck_icon.svg',
+            width: 50,
+            semanticsLabel: 'card',
+            colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+          ),
+          icon: SvgPicture.asset(
+            'assets/svg/deck_icon.svg',
+            width: 50,
+            semanticsLabel: 'card',
+            colorFilter:
+                const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+          ),
+        ),
       ],
     );
   }
-} 
+}
