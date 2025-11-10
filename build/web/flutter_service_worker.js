5033a96dde30843d1ee3d20c2bd926ebcc1c874d
Patricio Bruno Da Costa <pbrunorb@hotmail.com>
2025-11-10 11:55:17 -0300

    update build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0591ee08bece2c1ae21255e413f75316",
+const RESOURCES = {"flutter_bootstrap.js": "48409c4af21d6a8dd89966ff084f645f",
 "version.json": "719491c0973ca30b0eb15cc621c5cf8f",
 "splash/img/light-2x.png": "b6e651e169d8f9ddd4edc4307a666f89",
 "splash/img/branding-4x.png": "4c88385b264d3327b0beacd5b94354f7",
@@ -23,7 +23,7 @@ const RESOURCES = {"flutter_bootstrap.js": "0591ee08bece2c1ae21255e413f75316",
 "splash/img/branding-dark-3x.png": "f24ab21453ba6f61e3a4f79e4c46cea7",
 "index.html": "fe6a3a718635d49243996a0491a038bb",
 "/": "fe6a3a718635d49243996a0491a038bb",
-"main.dart.js": "ab3df4a9e5a49a714e12639ef81352ab",
+"main.dart.js": "4e0d88f119cd644acf496b7dbc4ab0fe",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "b15c0ae7ec13f83ef34c59aac39b7379",
 "icons/Icon-192.png": "fff3ddbf4a0108f84391d7e385055f15",
